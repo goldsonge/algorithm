523d4a5b7fd063ea0f3cacc470f883649c937f2c
goldsonge <124681132+goldsonge@users.noreply.github.com>
2024-09-12 09:20:48 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length+1];
-
-        for(int i=0; i<num_list.length; i++){
+        int length = num_list.length;
+        int[] answer = new int[length + 1];
+        for (int i = 0; i < length; i ++){
             answer[i] = num_list[i];
         }
-
-        answer[num_list.length] = num_list[num_list.length-1] > num_list[num_list.length-2] ? num_list[num_list.length-1]-num_list[num_list.length-2]:num_list[num_list.length-1]*2;
+        int last;
+        if(num_list[length - 1] > num_list[length - 2]){
+            last = num_list[length - 1] - num_list[length - 2];
+        } else {
+            last = 2 * num_list[length - 1];
+        }
+        answer[length] = last;
         return answer;
     }
 }
