0ac76ce757a3decf164783f28ec99783ee2bf3af
goldsonge <124681132+goldsonge@users.noreply.github.com>
2024-09-14 21:49:34 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int[] array, int height) {
-        int answer = 0;
-        return answer;
+        int number = 0;
+        for(int i = 0; i < array.length; i++) {
+            int[] answer = new int[array.length];
+            if (array[i] > height) {
+                number++;
+            }
+        }
+        return number;
     }
 }
