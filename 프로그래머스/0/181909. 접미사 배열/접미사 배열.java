6c906305b5543b968f0de84978b284b7cd0bcce1
goldsonge <124681132+goldsonge@users.noreply.github.com>
2024-09-18 14:53:40 +0900

    [level 0] Title: 접미사 배열, Time: 0.57 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int n = my_string.length();
+        
+        String[] suffixes = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            suffixes[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(suffixes);
+        
+        return suffixes;
+    }
+}
