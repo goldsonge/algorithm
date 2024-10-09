d98675c0f3433a1c4170011331911471b5beb581
goldsonge <124681132+goldsonge@users.noreply.github.com>
2024-10-10 00:53:52 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        if(a % 2 == 1 && b % 2 == 1){
+            return a*a + b*b;
+        } else if (a % 2 == 1 || b % 2 == 1){
+            return 2 * (a + b);
+        } else {
+            if(a > b){
+                return a - b;
+            } else {
+                return -(a - b);
+            }
+        }
+    }
+}
