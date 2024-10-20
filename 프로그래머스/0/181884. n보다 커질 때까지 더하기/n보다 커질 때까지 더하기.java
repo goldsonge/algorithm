a2da36c5e18f567ca464cb91e883a94c3132bfa1
goldsonge <124681132+goldsonge@users.noreply.github.com>
2024-10-20 23:13:34 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static int solution(int[] numbers, int n) {
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+            if (sum > n) {
+                return sum;
+            }
+        }
+        
+        return sum;
+    }
+}
