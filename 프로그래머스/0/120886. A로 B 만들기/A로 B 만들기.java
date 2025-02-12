ef245e441c30336a1dfed7b6189ce89989802c9e
goldsonge <124681132+goldsonge@users.noreply.github.com>
2025-02-12 16:39:31 +0900

    [level 0] Title: A로 B 만들기, Time: 0.97 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        if (before.length() != after.length()) {
+            return 0;
+        }
+        
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+        
+        return Arrays.equals(beforeArr, afterArr) ? 1 : 0;
+    }
+}
