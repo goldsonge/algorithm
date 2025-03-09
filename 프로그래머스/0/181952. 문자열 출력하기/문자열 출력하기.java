c6b5c805a6cc97f8399320060d91da8b7adc9bb6
goldsonge <124681132+goldsonge@users.noreply.github.com>
2025-03-09 23:15:58 +0900

    [level 0] Title: 문자열 출력하기, Time: 200.62 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+        scanner.close();
+    }
+}
