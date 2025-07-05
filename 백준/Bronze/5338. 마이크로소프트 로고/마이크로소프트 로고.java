5652355606867ee4ced82baf45901e6dc934b761
goldsonge <124681132+goldsonge@users.noreply.github.com>
2025-07-06 01:06:50 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
